8a8f446066c595f851bd10c3a3e3a60c277435dc
rodrigocan <rodrigocan@gmail.com>
2020-04-24 11:01:04 -0300

    Error: No metadata for Transaction was found.

---
+++ src/database/migrations/1587686017103-AddForeignKeyToColumnCategoryInTransactionsTable.ts
@@ -0,0 +1,48 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddForeignKeyToColumnCategoryInTransactionsTable1587686017103
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('transactions', 'category');
+
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionCategory',
+        columnNames: ['category'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+
+    await queryRunner.dropColumn('transactions', 'category');
+
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category',
+        type: 'varchar',
+      }),
+    );
+  }
+}
